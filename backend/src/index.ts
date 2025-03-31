051b0c5d9c75e428d32e716c3185c2f3a754bc41
Flo <73830951+floriane-perucchini@users.noreply.github.com>
2025-03-31 14:59:44 +0200

    Revert "init: docker for database and adminer and hot reload for backend"
    This reverts commit 284e32bf486244f334ee94ece7bd9d815f528bfe.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,7 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express pas mis à jour !");
-});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
