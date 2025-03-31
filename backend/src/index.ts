f898e6765ad847be8cd089a40dcefbc17bf7e9b2
Flo <73830951+floriane-perucchini@users.noreply.github.com>
2025-03-31 14:59:44 +0200

    Revert "init: frontend and backend"
    This reverts commit e66bd57e24ebd83a8e98bae9eace203d13dc1e67.

--- backend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import express, { Request, Response } from 'express';
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
