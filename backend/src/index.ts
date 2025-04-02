c80a3f34503de3fe0940cf7bb452dcb30147c004
valentinlpka <lipkavalentin@outlook.fr>
2025-04-02 14:59:23 +0200

    feat: configure cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,26 @@
-import express, { Request, Response } from "express";
+import cors from "cors";
+import express from "express";
+import swaggerUi from "swagger-ui-express";
 import router from "./routes/router";
-import swaggerUi from 'swagger-ui-express';
-import { specs } from './swagger';
+import { specs } from "./swagger";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-app.use('', router);
+app.use("", router);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
