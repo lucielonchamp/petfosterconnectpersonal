a29253c5c7968df32830f61829263fcbab88b0c7
lucielonchampoclock <lucie.lonchamp@oclock.school>
2025-04-22 16:30:51 +0200

    fix: condition id avant de delete un user

--- backend/src/controllers/foster.controller.ts
+++ backend/src/controllers/foster.controller.ts
@@ -7,131 +7,131 @@ import { createFosterSchema, updateFosterSchema } from "../schemas/foster.schema
 const prisma = new PrismaClient();
 
 export async function getFoster(request: Request, response: Response<ApiResponse<FosterWithUser[]> | ErrorResponse>): Promise<any> {
-    try {
-        const foster = await prisma.foster.findMany({
-            include: {
-                user: {
-                    select: {
-                        id: true,
-                        email: true,
-                        roleId: true
-                    }
-                },
-            },
-        });
+  try {
+    const foster = await prisma.foster.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            email: true,
+            roleId: true
+          }
+        },
+      },
+    });
 
-        if (!foster) {
-            return response.status(404).json({
-                success: false,
-                message: 'Families not found',
-                error: 'Families not found'
-            })
-        };
+    if (!foster) {
+      return response.status(404).json({
+        success: false,
+        message: 'Families not found',
+        error: 'Families not found'
+      })
+    };
 
-        return response.status(200).json({
-            success: true,
-            message: 'Families found',
-            data: foster
-        })
+    return response.status(200).json({
+      success: true,
+      message: 'Families found',
+      data: foster
+    })
 
-    } catch (error) {
-        return response.status(500).json({
-            success: false,
-            message: 'Error fetching foster families',
-            error: error
-        })
-    }
+  } catch (error) {
+    return response.status(500).json({
+      success: false,
+      message: 'Error fetching foster families',
+      error: error
+    })
+  }
 }
 
 export async function getFosterById(request: Request<{ id: string }>, response: Response<ApiResponse<FosterWithUser> | ErrorResponse>): Promise<any> {
-    const { id } = request.params;
+  const { id } = request.params;
 
-    if (!id) {
-        return response.status(400).json({
-            success: false,
-            message: 'Foster family ID is required'
-        })
+  if (!id) {
+    return response.status(400).json({
+      success: false,
+      message: 'Foster family ID is required'
+    })
+  }
+
+  try {
+    const foster = await prisma.foster.findUnique({
+      where: { id },
+      include: {
+        user: {
+          select: {
+            id: true,
+            email: true,
+            roleId: true
+          }
+        },
+      },
+    });
+
+    if (!foster) {
+      return response.status(404).json({
+        success: false,
+        message: 'Foster family not found'
+      })
     }
 
-    try {
-        const foster = await prisma.foster.findUnique({
-            where: { id },
-            include: {
-                user: {
-                    select: {
-                        id: true,
-                        email: true,
-                        roleId: true
-                    }
-                },
-            },
-        });
+    return response.status(200).json({
+      success: true,
+      message: `Foster family ${id} found`,
+      data: foster
+    })
 
-        if (!foster) {
-            return response.status(404).json({
-                success: false,
-                message: 'Foster family not found'
-            })
-        }
-
-        return response.status(200).json({
-            success: true,
-            message: `Foster family ${id} found`,
-            data: foster
-        })
-
-    } catch (error) {
-        return response.status(500).json({
-            success: false,
-            message: 'Error fetching foster family',
-            error: error
-        })
-    }
+  } catch (error) {
+    return response.status(500).json({
+      success: false,
+      message: 'Error fetching foster family',
+      error: error
+    })
+  }
 };
 
 export async function getAnimalsByFoster(request: Request<{ id: string }>, response: Response): Promise<any> {
-    const { id } = request.params;
+  const { id } = request.params;
 
-    if (!id) {
-        return response.status(400).json({
-            success: false,
-            message: 'Foster ID is required',
-        });
-    }
+  if (!id) {
+    return response.status(400).json({
+      success: false,
+      message: 'Foster ID is required',
+    });
+  }
 
-    try {
-        const fosterWithAnimals = await prisma.foster.findUnique({
-            where: { id },
-            include: {
-              animals: {
-                include: {
-                  specie: true,
-                },
-              },
-            },
-          });
+  try {
+    const fosterWithAnimals = await prisma.foster.findUnique({
+      where: { id },
+      include: {
+        animals: {
+          include: {
+            specie: true,
+          },
+        },
+      },
+    });
 
-        if (!fosterWithAnimals) {
-            return response.status(404).json({
-                success: false,
-                message: 'Foster Animal not found',
-                error: 'Foster Animal not found'
-            })
-        };
+    if (!fosterWithAnimals) {
+      return response.status(404).json({
+        success: false,
+        message: 'Foster Animal not found',
+        error: 'Foster Animal not found'
+      })
+    };
 
-        return response.status(200).json({
-            success: true,
-            message: 'Foster Animal found',
-            data: fosterWithAnimals
-        })
+    return response.status(200).json({
+      success: true,
+      message: 'Foster Animal found',
+      data: fosterWithAnimals
+    })
 
-    } catch (error) {
-        return response.status(500).json({
-            success: false,
-            message: 'Error fetching Foster Animal',
-            error: error
-        })
-    }
+  } catch (error) {
+    return response.status(500).json({
+      success: false,
+      message: 'Error fetching Foster Animal',
+      error: error
+    })
+  }
 }
 
 export async function createFoster(
@@ -180,68 +180,69 @@ export async function createFoster(
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Error creating foster", error });
-}
-
-export async function updateFoster(
-  req: Request<{ id: string }, {}, Partial<Omit<Foster, 'id' | 'createdAt' | 'updatedAt'>>>,
-  res: Response<ApiResponse<Foster> | ErrorResponse>
-): Promise<void> {
-  const { id } = req.params;
-  const requestBody = req.body;
-
-  const { success, data, error } = updateFosterSchema.safeParse(requestBody);
-
-  if (!success) {
-    res.status(400).json({ message: "Invalid data", error });
-    return;
   }
 
-  try {
-    const foster = await prisma.foster.update({
-      where: { id },
-      data
-    });
+  export async function updateFoster(
+    req: Request<{ id: string }, {}, Partial<Omit<Foster, 'id' | 'createdAt' | 'updatedAt'>>>,
+    res: Response<ApiResponse<Foster> | ErrorResponse>
+  ): Promise<void> {
+    const { id } = req.params;
+    const requestBody = req.body;
 
-    res.status(200).json({
-      success: true,
-      message: 'Foster updated successfully',
-      data: foster
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Error updating foster", error });
+    const { success, data, error } = updateFosterSchema.safeParse(requestBody);
+
+    if (!success) {
+      res.status(400).json({ message: "Invalid data", error });
+      return;
+    }
+
+    try {
+      const foster = await prisma.foster.update({
+        where: { id },
+        data
+      });
+
+      res.status(200).json({
+        success: true,
+        message: 'Foster updated successfully',
+        data: foster
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Error updating foster", error });
+    }
   }
-}
 
-export async function deleteFoster(request: Request<{ id: string }>, response: Response<ApiResponse<Foster> | ErrorResponse>): Promise<any> {
+  export async function deleteFoster(request: Request<{ id: string }>, response: Response<ApiResponse<Foster> | ErrorResponse>): Promise<any> {
 
     const { id } = request.params;
 
     try {
-        const deletedFoster = await prisma.foster.delete({
-            where: {
-                id
-            }
-        });
 
-        if (!id) {
-            return response.status(400).json({
-                success: false,
-                message: 'Foster family ID is required'
-            })
-        }
-
-        return response.status(200).json({
-            success: true,
-            message: `Foster Family ${id} deleted`,
-            data: deletedFoster
+      if (!id) {
+        return response.status(400).json({
+          success: false,
+          message: 'Foster family ID is required'
         })
+      }
+
+      const deletedFoster = await prisma.foster.delete({
+        where: {
+          id
+        }
+      });
+
+      return response.status(200).json({
+        success: true,
+        message: `Foster Family ${id} deleted`,
+        data: deletedFoster
+      })
 
     } catch (error) {
-        return response.status(500).json({
-            success: false,
-            message: 'Error deleting user',
-            error: error
-        })
+      return response.status(500).json({
+        success: false,
+        message: 'Error deleting user',
+        error: error
+      })
     }
-}
+  }
